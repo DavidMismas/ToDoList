4b0c1407bd7e5be02f821b23b06d61e6ff1b66bc
David Mismas <david.mismas@gmail.com>
2025-10-05 18:52:20 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by David Mišmaš on 5. 10. 25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
